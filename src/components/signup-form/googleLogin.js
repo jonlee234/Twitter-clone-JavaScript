2daf2b019cacfce2824278bfa3c442459c668fb3
David Sreesangkom <dsreesa@gmail.com>
2020-09-07 11:07:03 -0400

    google login but can't logout

--- src/components/signup-form/googleLogin.js
+++ src/components/signup-form/googleLogin.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React , {useState} from 'react';
 import { GoogleLogin } from 'react-google-login';
  
  
 export const GoogleLoginContainer = () => {
     
-  const responseGoogle = (response) => {
-      console.log(response)
-  }
 
+function App(){
+
+  const [name, setNAme] = useState("");
+  const [email, setEmail] = useState("");
+  const [url, setUrl] = useState("");
+}
+const responseGoogle = (response) => {
+  // setNAme(response.profileObj.name);
+  // setEmail(response.profileObj.email);
+  // setUrl(response.profileObj.imageUrl)
+}
     return (
       <React.Fragment>
+        {/* <div className = "App"></div> */}
+        <h1>Login with Google</h1>
+        {/* <h2>Welcome: {name}</h2> */}
+        {/* <h2>Email: {email}</h2> */}
+        {/* <img src = {url} alt ={name} /> */}
         <GoogleLogin
     clientId="166602026627-68pokdgg2o4g51lnnpjoj0ren91p6nnl.apps.googleusercontent.com"
     buttonText="Login"
